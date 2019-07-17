49fc5492d47449918f1d90f0f8b93d0e1f976c55
Stacy Johns <stacykjohns@icloud.com>
2019-07-17 22:50:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,6 @@ def countdown(counter)
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
-    return "HAPPY NEW YEAR!"
+    puts "HAPPY NEW YEAR!"
   end
 end
-
-
-def countdown_with_sleep

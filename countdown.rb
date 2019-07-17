a579c3400ca06e9eea10f7d02111164a62bf1f10
Stacy Johns <stacykjohns@icloud.com>
2019-07-17 22:41:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,7 @@
-def countdown("x")
-  x = 10
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x = x - 1
+def countdown(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
